faa3972a26c32a3ae44053f59979ea9d0b645785
María Paz Silva Matus <175349738+paz-dlv@users.noreply.github.com>
2025-11-26 00:19:51 -0300

    todo funcional, 26/11/25, 00:19

--- app/src/main/java/com/biblioapp/api/UploadService.kt
+++ app/src/main/java/com/biblioapp/api/UploadService.kt
@@ -8,14 +8,11 @@ import retrofit2.http.Part
 
 interface UploadService { // Declaramos una interfaz de Retrofit
 
-    @Multipart // Indicamos que la solicitud será multipart/form-data
-    @POST("upload/image") // Ruta del endpoint de subida (POST /upload/image)
-    suspend fun uploadImage( // Función suspend (corrutina) para subir la imagen
-        // El nombre del campo "content" en createFormData es el que Xano espera.
-        // Aquí el nombre del parámetro 'image' no importa.
+    @Multipart
+    @POST("upload/image")
+    suspend fun uploadImage(
+
         @Part image: MultipartBody.Part
-        // ¡¡¡CAMBIO CLAVE!!!
-        // La API devuelve un Array (una lista) de objetos de imagen, no un solo objeto.
-        // Cambiamos el tipo de retorno de 'UploadResponse' a 'List<ProductImage>'.
+
     ): List<ProductImage>
 }
